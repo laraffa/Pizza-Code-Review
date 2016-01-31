fdf61e7e9fd213b700a2a64325904920cff114b3
lindsey <lindsey.raffa@gmail.com>
2016-01-31 15:49:14 -0800

    pass spec to check for the size price of  a medium pizza

--- js/scripts.js
+++ js/scripts.js
@@ -8,5 +8,7 @@ function Pizza(toppings, size) {
 Pizza.prototype.sizePrice = function() {
   if (this.size === "small") {
     return 10;
+  } else if (this.size === "medium") {
+  return 15;
   }
 }
