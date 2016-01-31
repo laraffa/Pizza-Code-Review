ef384e864e2d94a212ce34b8bd0377f829448f19
lindsey <lindsey.raffa@gmail.com>
2016-01-31 15:41:25 -0800

    add first spec

--- spec/specs.js
+++ spec/specs.js
@@ -1,5 +1,8 @@
-// describe('helloWorld', function(){
-//   it("is going to be false", function(){
-//     expect(helloWorld()).to.equal(false);
-//   });
-// });
+
+describe('Pizza', function() {
+  it("creates a new pizza that describes for the following properties", function() {
+    var testPizza = new Pizza(1, "large");
+    expect(testPizza.size).to.equal("large");
+    expect(testPizza.toppings).to.equal(1);
+  });
+  });
